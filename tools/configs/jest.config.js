84518494d49f24087034b3e9b3d6a3982c65f146
水野光琉 <mizuno.hikaru.01@gmail.com>
2025-06-14 19:45:28 +0900

    setup:nuxt js

--- tools/configs/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.spec.js'],
-  coverageDirectory: '<rootDir>/coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
-};
